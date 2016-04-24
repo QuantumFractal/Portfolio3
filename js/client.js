4ebb9e9fdec559d209f4bf577bf74c4300d3fca0
Tom <absentisinaventulus@gmail.com>
2016-04-24 01:26:52 -0500

    Syncronized drawing now works

--- js/client.js
+++ js/client.js
@@ -10,20 +10,23 @@ clearBoard();
 var socket = io();
 currentStroke = {};
 
+
 /*
  * Generator functions
  */
-function Point(x, y){
-    return {'x':x,'y':y};
+function Point(x, y, type){
+    return {'x':x,'y':y, 'type':type};
 }
 
 /*
  * Socket Listeners
  */
-socket.on('stroke', drawStroke);
+socket.on('stroke', function(stroke){
+    console.log("having a stroke");
+    drawStroke(stroke);
+});
 
 socket.on('clear', clearBoard);
-
 
 /*
  * Canvas functions
@@ -36,6 +39,24 @@ function clearBoard() {
 
 function drawStroke(stroke) {
     
+    // Terrible solution
+    while(isMouseDown);
+
+    for (i=0; i<stroke.points.length; i++){
+        drawPoint(stroke.points[i]);
+    } 
+}
+
+function drawPoint(point) {
+    if (point.type === "start"){
+        context.beginPath();
+        context.moveTo(point.x, point.y);
+    } else if (point.type === "drag"){
+        context.lineTo(point.x, point.y);
+        context.stroke();
+    } else {
+        context.closePath();
+    }
 }
 
 function handleEvent(event) {
@@ -47,10 +68,14 @@ function handleEvent(event) {
         context.moveTo(mouseX, mouseY);
         
         // Save starting point
-        currentStroke = {'points':[Point(mouseX, mouseY)]};   
+        currentStroke = {'points':[Point(mouseX, mouseY, 'start')]};   
         
     } else if (event.type === "mouseup"){
         isMouseDown = false;
+        mouseX = event.offsetX;
+        mouseY = event.offsetY;
+        currentStroke.points.push(Point(mouseX, mouseY, 'end'));
+        
         
         // Send stroke
         socket.emit('stroke', currentStroke);
@@ -64,7 +89,7 @@ function handleEvent(event) {
             context.stroke();
             
             // Save intermediate points
-            currentStroke.points.push(Point(mouseX, mouseY));
+            currentStroke.points.push(Point(mouseX, mouseY, 'drag'));
         } 
     }
 }
