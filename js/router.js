1c94406d797a209985f8acb4e239f627630a7d59
Tom <absentisinaventulus@gmail.com>
2016-04-30 13:08:09 -0500

    Made join room work now

--- js/router.js
+++ js/router.js
@@ -8,12 +8,13 @@ socket.on('redirect', function(url) {
 var newRoomBtn  = document.getElementById("newRoomButton");
 newRoomBtn.addEventListener("click", function(evt){
     var name = document.getElementById("newInput").value;
-    
+    //console.log("BLAH");
     socket.emit('newRoom', name);
 });
 
 var joinRoomBtn  = document.getElementById("joinRoomButton");
-newRoomBtn.addEventListener("click", function(evt){
-    var room = document.getElementById("roomInput").value;
-    window.location = '/boards/'+url+'/';
+joinRoomBtn.addEventListener("click", function(evt){
+    var room = document.getElementById("joinInput").value;
+    console.log(room);
+    window.location = '/boards/'+room+'/';
 });
